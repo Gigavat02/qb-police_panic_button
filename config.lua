cd7773895e4427248fc5456d1cc82e9f54de72f4
Gigavat02 <76479107+Gigavat02@users.noreply.github.com>
2022-01-08 13:41:18 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.PanicCooldown = 0    -- The amount of time before an officer will be allowed to send an other alert (in seconds)
+Config.AlertTime = 60       -- How long the alert is going to show for (in seconds)
