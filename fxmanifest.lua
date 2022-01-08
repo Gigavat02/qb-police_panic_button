874306d7a2453243a14b8565f1f4cc7ca60a0cff
Gigavat02 <76479107+Gigavat02@users.noreply.github.com>
2022-01-08 13:40:31 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script {
+	"config.lua",
+	"client.lua"
+}
+
+server_script "server.lua"
+
+ui_page "html/index.html"
+
+files {
+	"html/index.html",
+	"html/sounds/*.ogg"
+}
